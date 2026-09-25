f5de08e2fa964288ed0668ee4f7f5b007f34a759
sshoney8756 <sshoney8756@gmail.com>
2026-09-25 16:18:48 +0900

    test: GameRound 저장 테스트

--- src/test/java/com/github/sshoney8756/clueless/RepositoryTest.java
+++ src/test/java/com/github/sshoney8756/clueless/RepositoryTest.java
@@ -3,7 +3,10 @@ package com.github.sshoney8756.clueless;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.sshoney8756.clueless.Domain.Game;
+import com.github.sshoney8756.clueless.Domain.GameRound;
 import com.github.sshoney8756.clueless.Repository.GameRepository;
+import com.github.sshoney8756.clueless.Repository.GameRoundRepository;
+import com.github.sshoney8756.clueless.common.GameRoundStatus;
 import com.github.sshoney8756.clueless.common.GameStatus;
 
 import jakarta.transaction.Transactional;
@@ -13,7 +16,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
@@ -21,22 +23,36 @@ import java.time.LocalDateTime;
 @DataJpaTest
 @Slf4j
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(locations = "classpath:application-test.properties")
 
-class GameRepositoryTest {
+class RepositoryTest {
 
     @Autowired
     GameRepository gameRepository;
 
+    @Autowired
+    GameRoundRepository gameRoundRepository;
+
     @Test
     @DisplayName("게임 저장")
-    @Rollback(value = false)
     @Transactional
     public void insertGame() {
         Game game = new Game(GameStatus.CREATED, 30, 1, LocalDateTime.now());
         Game savedGame = gameRepository.save(game);
         Game found = gameRepository.findById(game.getId()).orElseThrow();
+
         assertEquals(savedGame, found);
     }
 
+    @Test
+    @DisplayName("게임 라운드 저장")
+    @Transactional
+    public void insertGameRound() {
+        Game game = new Game(GameStatus.CREATED, 30, 1, LocalDateTime.now());
+        Game savedGame = gameRepository.save(game);
+        GameRound round = new GameRound(1, GameRoundStatus.CREATED, 1, 1, 5, LocalDateTime.now(), game);
+        GameRound savedRound = gameRoundRepository.save(round);
+
+        GameRound found = gameRoundRepository.findById(savedRound.getId()).orElseThrow();
+        assertEquals(found.getGame(), savedGame);
+    }
 }
