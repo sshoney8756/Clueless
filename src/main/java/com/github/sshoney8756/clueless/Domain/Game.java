65165114d77598184785552d75cda5da40191285
sshoney8756 <sshoney8756@gmail.com>
2026-09-25 14:37:10 +0900

    feat: Game 엔티티 속성 추가 및 수정

--- src/main/java/com/github/sshoney8756/clueless/Domain/Game.java
+++ src/main/java/com/github/sshoney8756/clueless/Domain/Game.java
@@ -1,32 +1,44 @@
 package com.github.sshoney8756.clueless.Domain;
 
 import com.github.sshoney8756.clueless.common.GameStatus;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
-@Getter @Setter
+@Getter
+@NoArgsConstructor
 public class Game {
 
-    @Id @GeneratedValue(generator = "uuid")
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private GameStatus status;
 
+    @Column(nullable = false)
     private int cluePoint;
+
+    @Column(nullable = false)
     private int currentRound;
+
     private int finalScore;
 
-    @Column(columnDefinition = "TIMESTAMP")
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
     private LocalDateTime startedAt;
 
     @Column(columnDefinition = "TIMESTAMP")
-    @Nullable
     private LocalDateTime endedAt;
 
+    public Game(GameStatus status, int cluePoint, int currentRound, LocalDateTime startedAt ) {
+        this.status = status;
+        this.cluePoint = cluePoint;
+        this.currentRound = currentRound;
+        this.startedAt = startedAt;
+    }
 }
--- src/main/java/com/github/sshoney8756/clueless/Repository/GameRepository.java
+++ src/main/java/com/github/sshoney8756/clueless/Repository/GameRepository.java
@@ -3,7 +3,9 @@ package com.github.sshoney8756.clueless.Repository;
 import com.github.sshoney8756.clueless.Domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
 
-public interface GameRepository extends JpaRepository<Game, String> {
+
+public interface GameRepository extends JpaRepository<Game, UUID> {
 
 }
